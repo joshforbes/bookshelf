e8efe1f1e7fc12e32008a10d8970d7aacb5a87a9
Josh Forbes <jforbes@kimmel.com>
2017-08-28 14:08:43 -0400

    Add simple validation for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  validates :isbn, :title, :description, presence: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  
+  describe 'Validation' do
+    before(:each) do
+      @book = build(:book)
+    end
+
+    it 'is valid with valid attributes' do
+      expect(@book).to be_valid
+    end
+
+    it 'is not valid without a isbn' do
+      @book.isbn = nil
+      expect(@book).not_to be_valid
+    end
+
+    it 'is not valid without a title' do
+      @book.title = nil
+      expect(@book).not_to be_valid
+    end
+
+    it 'is not valid without a description' do
+      @book.title = nil
+      expect(@book).not_to be_valid
+    end
+  end
 end
