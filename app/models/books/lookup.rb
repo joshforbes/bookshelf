fe1b349677fdf215c43b27be4fdef74578136de7
Josh Forbes <jforbes@kimmel.com>
2017-08-29 07:59:45 -0400

    Move the result classes back into lookup as private classes

--- app/models/books/lookup.rb
+++ app/models/books/lookup.rb
@@ -7,9 +7,39 @@ class Books::Lookup
     book = @searcher.search(isbn: isbn).first
 
     if book.isbn == isbn.to_s
-      Books::LookupResult.new(book)
+      Result.new(book)
     else
-      Books::NullLookupResult.new
+      NullResult.new
+    end
+  end
+
+  private
+
+  class Result
+    attr_reader :isbn, :title, :description, :pages, :published_date, :authors, :categories
+
+    def initialize(book)
+      @isbn = book.isbn
+      @title = book.title
+      @description = book.description
+      @pages = book.page_count
+      @published_date = book.published_date
+      @authors = book.authors_array
+      @categories = book.categories.split(/\s*,\s*/)
+    end
+
+    def exists?
+      true
+    end
+  end
+
+  class NullResult
+    def exists?
+      false
+    end
+
+    def method_missing(method, *args, &block)
+      nil
     end
   end
 end
--- spec/support/fake_lookup.rb
+++ spec/support/fake_lookup.rb
@@ -4,7 +4,8 @@ class FakeLookup
   end
 
   def by_isbn(isbn)
-    Books::LookupResult.new(OpenStruct.new(
+    OpenStruct.new(
+      exists?: true,
       isbn: isbn,
       title: 'Rspec',
       description: 'A book about Rspec',
@@ -12,6 +13,6 @@ class FakeLookup
       published_date: '2010',
       authors_array: ['David Chelimsky', 'Dan North'],
       categories: 'Computers'
-    ))
+    )
   end
 end
