8d0ec15626822bbedd4c7d514cc55f91caf2ff8c
Josh Forbes <jforbes@kimmel.com>
2017-08-30 10:30:02 -0400

    A method to add authors to a book

--- app/models/books/book.rb
+++ app/models/books/book.rb
@@ -3,5 +3,10 @@ module Books
     validates :isbn, :title, :description, presence: true
 
     has_and_belongs_to_many :authors, class_name: 'Authors::Author'
+
+    def add_author(name)
+      self.authors << Authors::Author.find_or_create_by(name: name)
+      self
+    end
   end
 end
