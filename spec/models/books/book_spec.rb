1796f5b24072eb2d9cf03c99cb1096518689f3d0
Josh Forbes <jforbes@kimmel.com>
2017-08-30 10:33:55 -0400

    Clean up spec

--- spec/models/books/book_spec.rb
+++ spec/models/books/book_spec.rb
@@ -26,31 +26,29 @@ RSpec.describe Books::Book, type: :model do
     end
   end
 
-  describe '#add_author?' do
-    before(:each) do
-      @book = create(:book)
-    end
+  describe '#add_author' do
+    let(:book) { create(:book) }
 
     it 'adds an author to the book' do
-      @book.add_author('Test Author')
+      book.add_author('Test Author')
 
-      expect(@book.authors.count).to eq(1)
-      expect(@book.authors.first.name).to eq('Test Author')
+      expect(book.authors.count).to eq(1)
+      expect(book.authors.first.name).to eq('Test Author')
     end
 
     it 'can add a second author' do
-      @book.add_author('Test Author')
-      @book.add_author('Another Author')
+      book.add_author('Test Author')
+      book.add_author('Another Author')
 
-      expect(@book.authors.count).to eq(2)
-      expect(@book.authors.second.name).to eq('Another Author')
+      expect(book.authors.count).to eq(2)
+      expect(book.authors.second.name).to eq('Another Author')
     end
 
-    it 'saves an author to the book that already exists but does not create a new author record' do
+    it 'adds an author to the book that already exists but does not create a new author record' do
       author = create(:author)
-      @book.add_author(author.name)
+      book.add_author(author.name)
 
-      expect(@book.authors.first.name).to eq(author.name)
+      expect(book.authors.first.name).to eq(author.name)
       expect(Authors::Author.count).to eq(1)
     end
   end
