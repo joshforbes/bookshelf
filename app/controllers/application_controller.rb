e0d43c090591f90ec0bfc92f6d40d170ce770a06
Josh Forbes <jforbes@kimmel.com>
2017-09-01 14:57:43 -0400

    Globally handle not found exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { errors: e.message }, status: :not_found
+  end
+
   def render_validation_error(object)
     render json: { errors: object.errors }, status: :unprocessable_entity
   end
