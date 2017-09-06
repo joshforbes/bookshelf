e3d8890f4d4f76df6a0800dc632bd4f0cc8c6545
Josh Forbes <jforbes@kimmel.com>
2017-09-06 13:18:59 -0400

    Validation rules for user

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -1,5 +1,9 @@
 module Users
   class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, uniqueness: true
+    validates :password, length: { minimum: 8 }, allow_nil: true
+    validates :display_name, uniqueness: true, allow_nil: true
   end
 end
--- spec/models/users/user_spec.rb
+++ spec/models/users/user_spec.rb
@@ -1,4 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Users::User, type: :model do
+  describe '#valid?' do
+    before(:each) do
+      @user = build(:user)
+    end
+
+    it 'is valid with valid attributes' do
+      expect(@user).to be_valid
+    end
+
+    it 'is valid with a new valid password' do
+      @user.password = 'New Password'
+
+      expect(@user).to be_valid
+    end
+
+    it 'is not valid without a password' do
+      @user.password = nil
+      expect(@user).not_to be_valid
+    end
+
+    it 'is not valid with a short password' do
+      @user.password = 'short'
+      expect(@user).not_to be_valid
+    end
+
+    it 'is not valid if email is not unique' do
+      another_user = create(:user)
+      @user.email = another_user.email
+      expect(@user).not_to be_valid
+    end
+
+    it 'is not valid if display_name is not unique' do
+      another_user = create(:user)
+      @user.display_name = another_user.display_name
+      expect(@user).not_to be_valid
+    end
+  end
 end
