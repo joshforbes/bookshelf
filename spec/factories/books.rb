f4eb49d76467e7676f4fffb3b56ebd021ca5973e
Josh Forbes <jforbes@kimmel.com>
2017-09-08 21:08:27 -0400

    Add a trait to create a rental with a book

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -5,5 +5,11 @@ FactoryGirl.define do
     description { Faker::Lorem.paragraph }
     pages { Faker::Number.number(3) }
     published_year { Faker::Number.between(1900, 2017) }
+
+    trait :with_rental do
+      after(:create) do |book|
+        create(:rental, book: book)
+      end
+    end
   end
 end
