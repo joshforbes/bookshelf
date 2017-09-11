59dbdc85c2410e599e345ae5904772980d9b381f
Josh Forbes <jforbes@kimmel.com>
2017-09-10 20:53:48 -0400

    Get rid of strong params here as it is not really buying us anything

--- app/controllers/rentals/rentals_controller.rb
+++ app/controllers/rentals/rentals_controller.rb
@@ -1,19 +1,13 @@
 module Rentals
   class RentalsController < ApplicationController
     def create
-      checkout = Rentals::Checkout.new(rental_params.merge(user: current_user))
+      checkout = Rentals::Checkout.new({ book_id: params[:book_id], user: current_user })
 
       if checkout.save
         render json: checkout.rental, status: :created
       else
         render_validation_error checkout
       end
-    end
-
-    private
-
-    def rental_params
-      params.permit(:book_id)
     end
   end
 end
