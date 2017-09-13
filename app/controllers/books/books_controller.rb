bfd385853ff6fa3a0fbd06436ec21109b4bea6e5
Josh Forbes <jforbes@kimmel.com>
2017-09-13 16:09:17 -0400

    Scaffold the index search for books

--- app/controllers/books/books_controller.rb
+++ app/controllers/books/books_controller.rb
@@ -1,5 +1,16 @@
 module Books
   class BooksController < ApplicationController
+    skip_before_action :authenticate!, only: [:index]
+
+    def index
+      search_term = params[:search_term].blank? ? '*' : params[:search_term]
+
+      results = Books::Search.search search_term, order: { created_at: :desc }
+
+      pp results
+      render json: results, status: :ok
+    end
+
     def create
       authorize Books::Book
       form = Books::AddBookForm.new(params.permit(:isbn))
--- spec/requests/books/books_spec.rb
+++ spec/requests/books/books_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'Books API', type: :request do
+RSpec.describe 'Books API', type: :request, search: true do
+  describe 'GET #index' do
+    it 'returns books when no search term is specified' do
+      book = create(:book, title: 'example')
+      SearchHelpers.reindex_search(Books::Search)
+
+      get books_url, params: {  }
+      puts json_response
+
+      book_id = json_response
+                  .fetch(:book)
+                  .fetch(:id)
+                  .to_i
+
+      expect(book_id).to eq(book.id)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
   describe 'POST #create' do
     before :each do
       stub_const('Books::Lookup', FakeLookup)
