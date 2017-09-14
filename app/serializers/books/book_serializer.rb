d0925b6a426d7464f10d1e888ec37da698456f3c
Josh Forbes <jforbes@kimmel.com>
2017-09-14 10:31:15 -0400

    Return the owner from the relationship

--- app/serializers/books/book_serializer.rb
+++ app/serializers/books/book_serializer.rb
@@ -1,7 +1,9 @@
 module Books
   class BookSerializer < ActiveModel::Serializer
-    attributes :id, :owner_id, :isbn, :title, :description, :pages,
+    attributes :id, :isbn, :title, :description, :pages,
                :published_year, :cover_image_url, :created_at, :authors, :tags
+
+    has_one :owner, serializer: Users::OwnerSerializer
 
     def authors
       object.authors.map(&:name)
