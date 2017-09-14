f19c8836a2f5a6160c6700a65d0a1ef7f77c5b3e
Josh Forbes <jforbes@kimmel.com>
2017-09-14 09:35:50 -0400

    Simplify the test double creation

--- spec/models/books/lookup_spec.rb
+++ spec/models/books/lookup_spec.rb
@@ -17,8 +17,7 @@ describe Books::Lookup do
     end
 
     it 'finds a book by a valid isbn' do
-      searcher = double('search')
-      allow(searcher).to receive(:search) { search_results }
+      searcher = double(search: search_results)
       google_books_search = Books::Lookup.new(searcher)
 
       book_result = google_books_search.by_isbn('9781934356371')
